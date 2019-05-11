237f9bd474edbfe18262c9720bfe749fa2d827e0
rohit-26 <rohit-26>
2019-05-11 10:27:09 +0530

    First commit

---
+++ src/main/java/org/practice/lambda/javaeight/App.java
@@ -0,0 +1,13 @@
+package org.practice.lambda.javaeight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
