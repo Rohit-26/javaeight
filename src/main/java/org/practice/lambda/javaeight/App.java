7f6bc6bf4cf06e1e011172966605470caa23c505
Rohit Tiwari <rohit.professional26@gmail.com>
2019-05-11 22:52:22 +0530

    Update App.java

--- src/main/java/org/practice/lambda/javaeight/App.java
+++ src/main/java/org/practice/lambda/javaeight/App.java
@@ -8,10 +8,10 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Apple");
-
+		
+		
 		System.out.println("Rohit Tiwari");
-		System.out.println("Mohit Tiwari");
-		System.out.println("Rohit Tiwari");
+		System.out.println("2+2");
 		
 
 	}
